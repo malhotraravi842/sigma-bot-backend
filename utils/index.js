b8ebf356723b054926002c81d23c508c184bc1e4
Ravi Chaudhary <ravi@lcx.com>
2023-07-15 23:27:57 +0530

    bug fixes

--- utils/index.js
+++ utils/index.js
@@ -7,23 +7,23 @@ exports.formatTextDetectionFromDocumentApiRes = (json) => {
 
   json.responses.forEach((res, pageNum) => {
     let data = res.fullTextAnnotation;
-    let pages = data?.pages;
-    let parsedText = data?.text;
+    let pages = data.pages;
+    let parsedText = data.text;
 
     let newWordsArr = [];
     pages.forEach((data) => {
-      let blocks = data?.blocks;
+      let blocks = data.blocks;
       blocks.forEach((block) => {
-        let paragraphs = block?.paragraphs;
+        let paragraphs = block.paragraphs;
         paragraphs.forEach((para) => {
-          let words = para?.words;
+          let words = para.words;
           words.forEach((word) => {
-            let normalizedVertices = word?.boundingBox?.normalizedVertices;
-            let symbols = word?.symbols;
+            let normalizedVertices = word.boundingBox.normalizedVertices;
+            let symbols = word.symbols;
 
             let wordString = "";
             symbols.forEach((symbol) => {
-              wordString += symbol?.text;
+              wordString += symbol.text;
             });
 
             newWordsArr.push({ normalizedVertices, word: wordString });
